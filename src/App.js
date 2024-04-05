4c1c5b9b5e80e2b6b5a241ccdc1c6984cceadffd
localbusdriver <83098547+localbusdriver@users.noreply.github.com>
2024-04-05 23:11:22 +1300

    Release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Button, ButtonGroup } from "react-bootstrap";
+
+import SummaryReporter from "./components/SummaryReporter";
+import DoubleOrders from "./components/DoubleOrders";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [func, setFunc] = useState("summary-reporter");
+  const [doubleOrderState, setDoubleOrderState] = useState({
+    1: { noDoubles: "Please Upload File" },
+  });
+  const [summaryState, setSummaryState] = useState({
+    1: { noSummary: "Please Paste Orders." },
+  });
+  const [fs, setFs] = useState(18);
+  useEffect(() => {
+    setFunc("summary-reporter");
+    // setFunc("double-order");
+  }, []);
+
+  const handleFuncChange = (e) => {
+    const selection = e.target.innerText.toLowerCase().replace(" ", "-");
+    setFunc(selection);
+  };
+
+  const handleFsIncrement = (e) => {
+    if (e.target.innerText === "+") {
+      setFs((prev) => prev + 4);
+    } else {
+      setFs((prev) => (prev > 4 ? prev - 4 : prev));
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>O'Sushi Summary Reporter</h1>
+        <ButtonGroup className="mt-3">
+          <Button
+            variant={func === "summary-reporter" ? "primary" : "secondary"}
+            onClick={handleFuncChange}
+          >
+            Summary Reporter
+          </Button>
+          <Button
+            variant={func === "double-order" ? "primary" : "secondary"}
+            onClick={handleFuncChange}
+          >
+            Double Order
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup className="mt-3">
+          <label className="mr-5">Font Size:&nbsp;&nbsp;</label>
+          <Button variant="danger" onClick={handleFsIncrement}>
+            -
+          </Button>
+          <Button variant="primary" onClick={handleFsIncrement}>
+            +
+          </Button>
+        </ButtonGroup>
       </header>
+      <main>
+        {func === "summary-reporter" ? (
+          <SummaryReporter
+            resultState={summaryState}
+            setResultState={setSummaryState}
+            fs={fs}
+          />
+        ) : (
+          <DoubleOrders
+            resultState={doubleOrderState}
+            setResultState={setDoubleOrderState}
+            fs={fs}
+          />
+        )}
+      </main>
     </div>
   );
 }
