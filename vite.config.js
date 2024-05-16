ea2a3a92f667268e64e6e831d0e37d346083130e
James La <jamesla200210@gmail.com>
2024-05-17 04:30:21 +1200

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,12 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-import commonjs from "@rollup/plugin-commonjs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  define: {
-    "process.env": {},
-  },
-  plugins: [react(), commonjs()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
